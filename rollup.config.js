c7a7e41424ecf7972ee955d293fcee65433d52a2
Hanks <zhanghan.me@gmail.com>
2017-06-05 16:03:02 +0800

    use rollup instead of webpack

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import buble from 'rollup-plugin-buble'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+export default {
+  entry: 'index.es6.js',
+  format: 'cjs',
+  dest: 'index.js',
+  plugins: [
+    nodeResolve({ jsnext: true, main: true }),
+    commonjs(),
+    buble({ transforms: { forOf: false } })
+  ]
+};
