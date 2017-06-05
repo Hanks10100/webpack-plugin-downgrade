94a3289dd34f03d34b704188b7f4c03624259ef0
Hanks <zhanghan.me@gmail.com>
2017-06-05 16:03:48 +0800

    add basic banner plugin

--- index.es6.js
+++ index.es6.js
@@ -1,1 +1,37 @@
 import Downgrade from '@weex-project/downgrade'
+import { ConcatSource } from 'webpack-sources'
+import ModuleFilenameHelpers from 'webpack/lib/ModuleFilenameHelpers'
+
+const defaultCondition = {
+
+}
+
+function generateDowngradeCode (options) {
+  const condition = options.condition || defaultCondition
+  return '// TODO: generate downgrade codes here\n'
+}
+
+export default class WeexDowngradePlugin {
+  constructor (options) {
+    this.options = options || {}
+  }
+
+  apply (compiler) {
+    const code = generateDowngradeCode(this.options)
+
+    compiler.plugin('compilation', (compilation) => {
+      compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+        chunks.forEach((chunk) => {
+          if ('isInitial' in chunk && !chunk.isInitial()) return;
+
+          chunk.files.forEach(file =>
+            compilation.assets[file] = new ConcatSource(
+              code.replace(/\[filename\]/g, file), '\n', compilation.assets[file]
+            )
+          )
+        })
+        callback()
+      })
+    })
+  }
+}
