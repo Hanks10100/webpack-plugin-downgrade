5277cd84f6fe33522e5cf465b34e77699a02b0ab
Hanks <zhanghan.me@gmail.com>
2017-06-27 11:24:45 +0800

    add test cases

---
+++ test/testHelper.js
@@ -0,0 +1,21 @@
+module.exports = {
+  setGlobalEnvironment () {
+    global.WXEnvironment = {}
+    global.weex = {
+      requireModule () {
+        return {
+          error () {
+            return true
+          }
+        }
+      }
+    }
+    global.callNative = function(){}
+  },
+
+  resetGlobalEnvironment () {
+    global.WXEnvironment = undefined
+    global.weex = undefined
+    global.callNative = undefined
+  }
+}
